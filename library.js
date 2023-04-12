77338f8d03c8111785f4dd689e6c5d730be75991
roopesh4p5 <roopesh4p5@gmail.com>
2023-04-12 21:29:24 +0530

    first

---
+++ library.js
@@ -0,0 +1,8 @@
+export let msg = `this is an example for import and export`
+
+
+
+ export let sum=(a,b)=>{
+    let add=a+b;
+console.log(add)
+}
---
+++ main.js
@@ -0,0 +1,4 @@
+import {msg,sum} from "./library.js"
+console.log(msg)
+document.getElementById('print').innerHTML=msg
+sum(10,20); 
